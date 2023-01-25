6519fadaf57b349617e61bdb7fa12eb492c35935
calixtonascimento <calixtoalbea@gmail.com>
2023-01-24 21:35:31 -0300

    add: first commit

---
+++ assets/arquivo_imoveis.md.1dff8996.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as a,o as e,a as n}from"./app.00c25657.js";const y=JSON.parse('{"title":"Imóveis","description":"","frontmatter":{},"headers":[],"relativePath":"arquivo/imoveis.md"}'),o={name:"arquivo/imoveis.md"},t=n("",3),l=[t];function p(c,i,r,d,D,F){return e(),a("div",null,l)}const _=s(o,[["render",p]]);export{y as __pageData,_ as default};
--- assets/arquivo_imoveis.md.50a41b38.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as a,o as e,a as n}from"./app.384b2e40.js";const y=JSON.parse('{"title":"Imóveis","description":"","frontmatter":{},"headers":[],"relativePath":"arquivo/imoveis.md"}'),o={name:"arquivo/imoveis.md"},t=n("",3),l=[t];function p(c,i,r,d,D,F){return e(),a("div",null,l)}const _=s(o,[["render",p]]);export{y as __pageData,_ as default};
