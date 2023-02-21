762e076237609bce9a66f28f41ceb75950dbffd4
tecnologiacnm <tecnologia@chavesnamao.com.br>
2023-02-21 16:43:39 -0300

    chore: last updated

--- assets/arquivo_encoding.md.a0775501.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,c as e,o as n,a as o}from"./app.dd797cc2.js";const D=JSON.parse('{"title":"Encoding","description":"","frontmatter":{},"headers":[],"relativePath":"arquivo/encoding.md"}'),s={name:"arquivo/encoding.md"},t=o("",4),r=[t];function c(l,p,i,d,_,h){return n(),e("div",null,r)}const u=a(s,[["render",c]]);export{D as __pageData,u as default};
